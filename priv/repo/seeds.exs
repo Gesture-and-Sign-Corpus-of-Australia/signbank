317c12e39e7916fb217dcd470a4180cb4d624f9e
Derek Cao <derek.jcao@gmail.com>
2025-11-25 17:20:01 +1100

    fix: italicised publication titles

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,66 +10,66 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-house =
-  Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
-    id_gloss: "house1a",
-    definitions: [
-      %Signbank.Dictionary.Definition{
-        text:
-          "A building in which people live. English = house. Formal English = dwelling, residence.",
-        pos: 0,
-        role: :noun,
-        published: true
-      }
-    ],
-    keywords: [
-      %Signbank.Dictionary.SignKeyword{
-        text: "house"
-      },
-      %Signbank.Dictionary.SignKeyword{
-        text: "home"
-      }
-    ],
-    type: :citation,
-    id_gloss_annotation: "HOUSE",
-    published: true,
-    morphology: %{},
-    phonology: %{}
-  })
+# house =
+#   Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
+#     id_gloss: "house1a",
+#     definitions: [
+#       %Signbank.Dictionary.Definition{
+#         text:
+#           "A building in which people live. English = house. Formal English = dwelling, residence.",
+#         pos: 0,
+#         role: :noun,
+#         published: true
+#       }
+#     ],
+#     keywords: [
+#       %Signbank.Dictionary.SignKeyword{
+#         text: "house"
+#       },
+#       %Signbank.Dictionary.SignKeyword{
+#         text: "home"
+#       }
+#     ],
+#     type: :citation,
+#     id_gloss_annotation: "HOUSE",
+#     published: true,
+#     morphology: %{},
+#     phonology: %{}
+#   })
 
-Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
-  id_gloss: "house1b",
-  type: :variant,
-  id_gloss_annotation: "HOUSE",
-  keywords: [
-    %Signbank.Dictionary.SignKeyword{
-      text: "house"
-    },
-    %Signbank.Dictionary.SignKeyword{
-      text: "home"
-    }
-  ],
-  published: true,
-  variant_of_id: house.id,
-  morphology: %{},
-  phonology: %{}
-})
+# Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
+#   id_gloss: "house1b",
+#   type: :variant,
+#   id_gloss_annotation: "HOUSE",
+#   keywords: [
+#     %Signbank.Dictionary.SignKeyword{
+#       text: "house"
+#     },
+#     %Signbank.Dictionary.SignKeyword{
+#       text: "home"
+#     }
+#   ],
+#   published: true,
+#   variant_of_id: house.id,
+#   morphology: %{},
+#   phonology: %{}
+# })
 
-Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
-  id_gloss: "dog",
-  type: :citation,
-  id_gloss_annotation: "DOG",
-  keywords: [
-    %Signbank.Dictionary.SignKeyword{
-      text: "dog"
-    }
-  ],
-  published: true,
-  morphology: %{},
-  phonology: %{}
-})
+# Signbank.Repo.insert!(%Signbank.Dictionary.Sign{
+#   id_gloss: "dog",
+#   type: :citation,
+#   id_gloss_annotation: "DOG",
+#   keywords: [
+#     %Signbank.Dictionary.SignKeyword{
+#       text: "dog"
+#     }
+#   ],
+#   published: true,
+#   morphology: %{},
+#   phonology: %{}
+# })
 
-Signbank.Repo.insert!(%Signbank.Accounts.User{
-  email: "test123@gmail.com",
-  role: :tech
-})
+# Signbank.Repo.insert!(%Signbank.Accounts.User{
+#   email: "test123@gmail.com",
+#   role: :tech
+# })
