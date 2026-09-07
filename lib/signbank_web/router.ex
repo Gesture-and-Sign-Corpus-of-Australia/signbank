6ce0f8db254481ee546e2eeb0b697a49b2d992c5
River <22485304+r-tae@users.noreply.github.com>
2026-09-07 16:08:53 +1000

    Readd search route

--- lib/signbank_web/router.ex
+++ lib/signbank_web/router.ex
@@ -39,6 +39,7 @@ defmodule SignbankWeb.Router do
     live "/dictionary/phonological-search", SignLive.PhonologicalSearch, :show
 
     get "/dictionary/", PageController, :redirect_home
+    live "/dictionary/sign/", SignLive.Basic, :search
     live "/dictionary/sign/:id", SignLive.Basic, :show
     live "/dictionary/sign/:id/detail", SignLive.Detail, :show
 
